a542b92a59804043232c18698ebb433bffa75789
vvmaster <vvmaster@gmail.com>
2020-02-28 18:39:17 +0100

    added authentication filter

---
+++ tweet-consumer-proxy-service/src/main/java/com/challenge/tweetconsumerproxyservice/AuthenticationFilter.java
@@ -0,0 +1,66 @@
+/**
+ * 
+ */
+package com.challenge.tweetconsumerproxyservice;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import com.challenge.tweetconsumerproxyservice.domain.LoginUser;
+
+/**
+ * @author vvmaster
+ *
+ */
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class AuthenticationFilter implements Filter {
+
+	private Map<LoginUser, String> activeTokens = new HashMap<>();
+
+	public AuthenticationFilter() {
+	}
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+			throws IOException, ServletException {
+		
+		HttpServletRequest request = (HttpServletRequest) req;
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+		
+		if (httpRequest.getServletPath().endsWith("authenticate") ) {
+			chain.doFilter(req, res);
+			return;
+		}
+		
+		String oAuth = httpRequest.getHeader("oAuth");
+		
+		if (!activeTokens.containsValue(oAuth)) {
+			throw new ServletException("Error: Token invalido");
+		}
+
+		chain.doFilter(req, res);
+	}
+	
+	public String getToken(LoginUser user) {
+		return this.activeTokens.get(user);
+	}
+	
+	public void addToken(LoginUser user, String token) {
+		this.activeTokens.put(user, token);
+	}
+
+}

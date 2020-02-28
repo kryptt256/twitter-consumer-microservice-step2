adbbfc9847e537f57d5d9441ae9562fb8010f060
vvmaster <vvmaster@gmail.com>
2020-02-28 10:30:04 +0100

    agregando servicios: commit inicial

---
+++ tweet-consumer-proxy-service/src/main/java/com/challenge/tweetconsumerproxyservice/TweetConsumerProxyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.tweetconsumerproxyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweetConsumerProxyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweetConsumerProxyServiceApplication.class, args);
+	}
+
+}
---
+++ tweet-consumer-proxy-service/src/test/java/com/challenge/tweetconsumerproxyservice/TweetConsumerProxyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.tweetconsumerproxyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TweetConsumerProxyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
